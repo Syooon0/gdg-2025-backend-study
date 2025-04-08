ccea7dab25eb2158c87603ff8f545cf899eb39f7
정승윤 <jswun123@g.hongik.ac.kr>
2025-04-08 16:27:29 +0900

    feat: init project

---
+++ src/test/java/gdg/backend/Gdg2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdg2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
